24e79c845f4ade9c9acb057f1f8f4187e7842022
ken0901 <ken5260715@gmail.com>
2023-06-12 22:30:09 -0700

    Initial commit

---
+++ src/main/java/com/ken/app/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.ken.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
