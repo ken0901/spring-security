c7becbe1a50f2ed8b043fa1e2ddce4bc0b0583fb
ken0901 <ken5260715@gmail.com>
2023-06-14 22:03:47 -0700

    Storing user credential in memory

--- src/main/java/com/ken/app/config/SecurityConfig.java
+++ src/main/java/com/ken/app/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,6 +26,20 @@ public class SecurityConfig {
         http.httpBasic();
         http.csrf().disable();
         return http.build();
+    }
+
+    // User Credential
+    @Bean
+    public UserDetailsService  userDetailsService() {
+        var user = User.withUsername("ken")
+                .password("{noop}lee")
+                .roles("USER")
+                .build();
+        var admin = User.withUsername("katie")
+                .password("{noop}lee")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
     }
 
     // Cross-Origin Resource Sharing (CORS):
