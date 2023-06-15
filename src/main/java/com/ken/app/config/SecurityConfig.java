39319eb481543404bc6f09bb7126f2f4d284320d
ken0901 <ken5260715@gmail.com>
2023-06-14 22:27:47 -0700

    Storing User Credentials using JDBC and H2 Database

--- src/main/java/com/ken/app/config/SecurityConfig.java
+++ src/main/java/com/ken/app/config/SecurityConfig.java
@@ -2,12 +2,18 @@ package com.ken.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -25,10 +31,12 @@ public class SecurityConfig {
         //http.formLogin();
         http.httpBasic();
         http.csrf().disable();
+        http.headers().frameOptions().sameOrigin();
         return http.build();
     }
 
-    // User Credential
+    // User Credential in memory
+    /*
     @Bean
     public UserDetailsService  userDetailsService() {
         var user = User.withUsername("ken")
@@ -40,6 +48,34 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user,admin);
+    }
+    */
+
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    // User Credential in H2 Database depends on roles(admin,user)
+    @Bean
+    public UserDetailsService  userDetailsService(DataSource dataSource) {
+        var user = User.withUsername("ken")
+                .password("{noop}lee")
+                .roles("USER")
+                .build();
+        var admin = User.withUsername("katie")
+                .password("{noop}lee")
+                .roles("ADMIN")
+                .build();
+
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
     }
 
     // Cross-Origin Resource Sharing (CORS):
